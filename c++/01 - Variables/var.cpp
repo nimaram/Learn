c8f2a0b125c3484b5493d731f24002457aa62740
NimaAram1 <aramnima50@gmail.com>
2021-05-08 13:10:18 +0430

    `c++` basic hello world and variables

---
+++ c++/01 - Variables/var.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name = "majid";
+    string family = "majidi";
+    int point = 25;
+    cout << "Full name:" << name + " " + family << endl;
+}
