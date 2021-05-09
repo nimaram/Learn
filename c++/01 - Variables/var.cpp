9a9f7d36fc5cbbfd930c9774fffbcdffe9e16ffb
NimaAram1 <aramnima50@gmail.com>
2021-05-09 10:40:39 +0430

    `c++`: learned about variables, and got a basic hello world!

--- c++/01 - Variables/var.cpp
+++ c++/01 - Variables/var.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(){
     string name = "majid";
     string family = "majidi";
+    float score = 20.1;
+    bool isAdmin = false;
     int point = 25;
     cout << "Full name:" << name + " " + family << endl;
+    cout << score << endl;
 }
