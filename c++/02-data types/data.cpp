c40bcc1bf29a23970c0c68354de5c847fab06007
NimaAram1 <aramnima50@gmail.com>
2021-05-12 11:13:51 +0430

    learned about data types in c++

---
+++ c++/02-data types/data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // data types here
+  char K_alephbet = 'K';
+  string language = "c++";
+  int point = 20;
+  float complete_point = 20.52;
+  double score = 9.8;
+  bool isActive = true;
+  cout << "worked!" << isActive << endl;
+  return 0;
+
+}
